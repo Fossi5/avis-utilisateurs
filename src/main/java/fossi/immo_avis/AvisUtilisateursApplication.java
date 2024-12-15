05a203c71f8e06531360de425d59fc09470eba19
Fossi5 <fossijoel5@gmail.com>
2024-12-16 00:14:44 +0100

    commit1

---
+++ src/main/java/fossi/immo_avis/AvisUtilisateursApplication.java
@@ -0,0 +1,13 @@
+package fossi.immo_avis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvisUtilisateursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvisUtilisateursApplication.class, args);
+	}
+
+}
